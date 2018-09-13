9502f1f5ce468888bbcbe4b9da6b40f9a2700f4d
Dennis Dorsch <d.dorsch.ext@powercloud.de>
2018-09-13 11:25:28 +0200

    add part 2

---
+++ part2/src/main/java/com/cc/spring/springschulung_part2/SpringschulungPart2Application.java
@@ -0,0 +1,12 @@
+package com.cc.spring.springschulung_part2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringschulungPart2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringschulungPart2Application.class, args);
+	}
+}
